eb2e5a93f7a2d3c68cdcf5fb0e2013736be7ae07
Caio Taniguti <caio.taniguti06@gmail.com>
2024-10-22 21:50:45 -0300

    atividade em sala

---
+++ src/com/exemplo/pacote/Cachorro.java
@@ -0,0 +1,9 @@
+package com.exemplo.pacote;
+
+public class Cachorro extends Animal {
+    @Override
+    protected void fazerSom(){
+        System.out.println("Au Au");
+    }
+
+}
---
+++ src/com/outro/pacote/TesteOutroPacote.java
@@ -0,0 +1,13 @@
+package com.outro.pacote;
+
+import com.exemplo.pacote.Animal;
+
+public class TesteOutroPacote {
+    public static void main(String[] args){
+        Animal animal = new Animal();
+        //animal.setNome("Bobby");
+        //animal.fazerSom();
+    }
+}
+
+
